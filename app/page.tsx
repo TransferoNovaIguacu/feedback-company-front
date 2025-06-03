5892e90d89ff13a91785f95377b89f890630cf97
Alex Lanção <lancao.af@gmail.com>
2025-06-03 15:29:02 -0300

    refactor: remove HeroPage component from EmConstrucao page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Head from 'next/head';
-import HeroPage from './components/HeroPage';
 
 export default function EmConstrucao() {
 
@@ -45,8 +44,6 @@ export default function EmConstrucao() {
           &copy; 2023 Transfero Academy Nova Iguaçu. Todos os direitos reservados.
         </div>
       </div>
-
-      <HeroPage></HeroPage>
 
       <style jsx>{`
         .progress-bar .animate-progress {
