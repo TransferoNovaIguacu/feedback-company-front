5d2c8fa098a4cd8e643e1a38d19895fcab7d4a64
Mariana <mxrx0699@gmail.com>
2025-06-03 14:17:32 -0300

    style: Estilização dos botões da plataforma

--- app/components/Botao.tsx
+++ app/components/Botao.tsx
@@ -5,9 +5,7 @@ interface TituloProps {
 export function Botao1({ texto }: TituloProps) {
     return (
         <div>
-            <button className="bg-lime-600 hover:bg-lime-800 hover:scale-105 transition duration-200 text-white rounded-lg px-3 flex items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-            </svg>
+            <button className="bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold px-4 py-2 rounded-md hover:opacity-90 transition">
                 {texto}</button>
         </div>
 
@@ -17,9 +15,7 @@ export function Botao1({ texto }: TituloProps) {
 export function Botao2({ texto }: TituloProps) {
     return (
         <div>
-            <button className="bg-red-700 hover:bg-orange-900 hover:scale-105 transition duration-200 text-white rounded-lg px-3 flex items-center gap-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-            </svg>
+            <button className="bg-gray-700 text-white font-semibold px-4 py-2 rounded-md hover:bg-gray-600 transition">
                 {texto}</button>
         </div>
 
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,18 +1,10 @@
 import { Botao1, Botao2,} from "../components/Botao";
-import { Parag } from "../components/Paragrafo";
-import { Parag2 } from "../components/Paragrafo2";
-import { Rodape } from "../components/Rodape";
-import {Titulo,} from "../components/Titulo";
 
 export default function HomePage() {
     return (
-        <div className="bg-white h-screen w-screen p-2">
-            <Titulo texto="Mariana Santos"/>
-            <Parag/>
-            <Parag2/>
-            <Rodape/>
-            <Botao1 texto="Share your experience"/>
-            <Botao2 texto="Delete"/>
+        <div className="bg-blue-950 h-screen w-screen p-2">
+            <Botao1 texto="Entrar"/>
+            <Botao2 texto="Cadastrar"/>
         </div>
 
     )
