90051bf45acb3e5c687dbc3bbe76d21942c49a8a
Alex Lanção <lancao.af@gmail.com>
2025-06-05 17:00:04 -0300

    feat: update CompanyPage to display reports instead of construction message

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,7 +1,9 @@
 import EmConstrucao from "../en-construcao/page";
 
-export default function CompanyPage(){
-    return(
-        <EmConstrucao/>
-    )
+export default function CompanyPage() {
+  return (
+    <div className="h-screen w-screen">
+        <h1>Relatorios</h1>
+    </div>
+  );
 }
