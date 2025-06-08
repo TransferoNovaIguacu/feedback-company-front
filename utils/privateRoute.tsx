3f68ad4045beadb02a0f1aea7b7a7c2732742118
Alex Lanção <lancao.af@gmail.com>
2025-06-08 13:32:21 -0300

    refactor: streamline token verification logic in AuthGuard component

--- utils/privateRoute.tsx
+++ utils/privateRoute.tsx
@@ -2,22 +2,36 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-// import { isTokenExpired } from "@/utils/isTokenExpired";
+import api from "./axios";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const [authorized, setAuthorized] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem("TOKEN");
-    if (!token) {
-      router.push("/auth/login");
-    } else {
+  const verifyToken = async (token: string) => {
+    try {
+      const response = await api.post("auth/token/verify/", { token });
+      // Se a resposta for 200, consideramos autorizado
       setAuthorized(true);
+    } catch (error: any) {
+      setAuthorized(false);
+      router.push("/auth/login");
     }
+  };
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = localStorage.getItem("TOKEN");
+      if (!token) {
+        router.push("/auth/login");
+      } else {
+        await verifyToken(token);
+      }
+    };
+    checkToken();
   }, [router]);
 
-  if (!authorized) return null; // ou um loading spinner
+  if (!authorized) return null; // ou retornar um spinner de loading
 
   return <>{children}</>;
 }
